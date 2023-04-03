afdc032b37dd3489f80996de0328c0c515f9c670
JonSpradlin <129249383+JonSpradlin@users.noreply.github.com>
2023-04-02 21:14:51 -0700

    initial commit

---
+++ summon_ally/summon_ally/Assets/SummonAlly.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SummonAlly : MonoBehaviour
+{
+    public GameObject ally;
+    float timerStart;
+
+    void Start()
+    {
+        timerStart = Time.time - 5f;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            //Vector3 allyLoc = new Vector3(transform.localPosition.x - 0.65f, transform.localPosition.y - 0.65f, 0f);
+            Vector3 allyLoc = new Vector3(transform.localPosition.x - 1f, transform.localPosition.y - 1f, 0f);
+            ally.transform.localPosition = allyLoc;
+            timerStart = Time.time;
+        }
+
+        if (Time.time - timerStart < 5)
+        {
+            ally.transform.parent = transform;
+        }
+        else
+        {
+            ally.transform.parent = null;
+        }
+    }
+}
